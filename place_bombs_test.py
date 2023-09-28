336a4b7089517403d1c15cc8000efd3a16a428f9
crishard <crislantorres9@gmail.com>
2023-09-28 08:11:27 -0300

    teste numero de bombas negativo

--- place_bombs_test.py
+++ place_bombs_test.py
@@ -55,3 +55,10 @@ def test_place_bombs_no_extra_bombs():
     bomb_count = sum(row.count(-1) for row in field)
     assert bomb_count == bombs
 
+def test_place_bombs_negative_bombs():
+
+    field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    bombs = -1
+    place_bombs_function(field, bombs)
+    bomb_count = sum(row.count(-1) for row in field)
+    assert bomb_count == 0
