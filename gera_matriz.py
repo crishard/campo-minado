4c8766660ab1a356f0d53e772a64333cb015c206
crishard <crislantorres9@gmail.com>
2023-09-30 15:41:20 -0300

    Isolando função do popup de vitória

--- gera_matriz.py
+++ gera_matriz.py
@@ -15,23 +15,3 @@ def criar_matriz_aleatoria():
 matrizes = [criar_matriz_aleatoria() for _ in range(2)]
 
 print(matrizes[1])
-
-def calculate_numbers_function(field, rows, cols):
-    for row in range(rows):
-        for col in range(cols):
-            if field[row][col] == -1:
-                continue
-            bomb_count = 0
-            for dr in [-1, 0, 1]:
-                for dc in [-1, 0, 1]:
-                    if 0 <= row + dr < rows and 0 <= col + dc < cols:
-                        if field[row + dr][col + dc] == -1:
-                            bomb_count += 1
-            field[row][col] = bomb_count
-
-# Chamando a função para calcular os números
-calculate_numbers_function(matrizes[1], 5, 5)
-
-# Imprimindo o tabuleiro com os números calculados
-for row in matrizes[1]:
-    print(row)
