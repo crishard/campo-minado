a267b6a95c32aeac471a9c93a688e2851b37d3ce
crishard <crislantorres9@gmail.com>
2023-09-30 16:39:50 -0300

    refatorando funcao place_bombs

--- functions/place_bombs.py
+++ functions/place_bombs.py
@@ -1,6 +1,19 @@
+# No arquivo functions/place_bombs.py
+
 import random
 
 def place_bombs_function(field, bombs):
+    if field is None:
+        raise ValueError("Field cannot be None")
+    
+    bombs = int(bombs)
+    
+    empty_cells = sum(row.count(0) for row in field)
+    
+    # Verificar se há espaço suficiente para as bombas
+    if bombs > empty_cells:
+        bombs = empty_cells  # Coloque apenas o número de bombas que cabe no campo
+    
     bomb_count = 0
     while bomb_count < bombs:
         row = random.randint(0, len(field) - 1)
