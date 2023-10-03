084fb2080f0e44af185b57e87fa6b1210ec9102e
crishard <crislantorres9@gmail.com>
2023-10-03 09:33:32 -0300

    testes dos popus de vitoria e derrota: exibindo vitoria em 2 horas

--- show_victory_and_defeat_popup_test.py
+++ show_victory_and_defeat_popup_test.py
@@ -43,3 +43,14 @@ def test_victory_in_1_hour_30_minutes_15_seconds():
     assert isinstance(root.winfo_children()[0], tk.Toplevel)
     root.destroy()
 
+
+def test_victory_in_2_hours():
+    root = tk.Tk()
+
+    def show_difficulty_menu():
+        pass
+
+    victory_time = timedelta(hours=2, minutes=0, seconds=0)
+    show_victory_popup_function(victory_time, root, show_difficulty_menu)
+    assert isinstance(root.winfo_children()[0], tk.Toplevel)
+    root.destroy()
