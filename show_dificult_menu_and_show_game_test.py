00164885540c1bb588702376bd9a8165edd7fb31
crishard <crislantorres9@gmail.com>
2023-10-04 19:02:40 -0300

    Verificando se o tamanmho dos tabuleiros em todos os níveis vem totalmente cobertos

--- show_dificult_menu_and_show_game_test.py
+++ show_dificult_menu_and_show_game_test.py
@@ -61,9 +61,39 @@ def test_dimension_mid(root):
 
     assert game.rows == 10 and game.cols == 16
 
-    
+
 def test_dimension_hard(root): 
     show_difficulty_menu_function(root, show_game)
     game = show_game("Difícil", show_difficulty_menu_function, root)
 
     assert game.rows and game.cols == 24
+
+def test_matriz_easy(root):
+    show_difficulty_menu_function(root, show_game)
+    game = show_game("Fácil", show_difficulty_menu_function, root)
+
+    # Verifica se todas as células do tabuleiro estão cobertas
+    for row in range(game.rows):
+        for col in range(game.cols):
+            button = game.buttons[row][col]
+            assert isinstance(button, tk.Button)
+
+
+def test_matriz_mid(root):
+    show_difficulty_menu_function(root, show_game)
+    game = show_game("Intermediário", show_difficulty_menu_function, root)
+
+    # Verifica se todas as células do tabuleiro estão cobertas
+    for row in range(game.rows):
+        for col in range(game.cols):
+            button = game.buttons[row][col]
+            assert isinstance(button, tk.Button)
+def test_matriz_hard(root):
+    show_difficulty_menu_function(root, show_game)
+    game = show_game("Difícil", show_difficulty_menu_function, root)
+
+    # Verifica se todas as células do tabuleiro estão cobertas
+    for row in range(game.rows):
+        for col in range(game.cols):
+            button = game.buttons[row][col]
+            assert isinstance(button, tk.Button)
