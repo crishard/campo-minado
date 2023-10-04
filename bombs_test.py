94a76bb31323dd9f1fd2239539d4a1fbc98a4df8
crishard <crislantorres9@gmail.com>
2023-10-04 19:35:54 -0300

    Verificando se o tabuleiro criado contém somente bombas

--- bombs_test.py
+++ bombs_test.py
@@ -27,3 +27,17 @@ def test_bombs_game_easy_is_smaller(root):
     bombs_test = 5
 
     assert bombs_test < game.bombs
+
+
+def test_board_contains_only_bombs_easy(root):
+    show_difficulty_menu_function(root, show_game)
+    game = start_game(8, 8, 64, root, show_difficulty_menu_function(root, show_game))
+
+    # Verifica se todas as células do tabuleiro contêm bombas
+    for row in range(game.rows):
+        for col in range(game.cols):
+            if game.field[row][col] != -1:  # Se a célula não contém uma bomba
+                assert False, f"Célula ({row}, {col}) não contém uma bomba"
+
+    # Se todas as células foram verificadas e nenhuma falha ocorreu, o teste passou
+    assert True
