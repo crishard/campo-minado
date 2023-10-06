2083393c71755dea8def8b9fa9fa78be02ade36a
crishard <crislantorres9@gmail.com>
2023-10-06 10:37:49 -0300

    Testes Calculo de adjacencia: testando se existem bombas adjacentes ao click

--- tests/adjacent_bombs_test.py
+++ tests/adjacent_bombs_test.py
@@ -30,3 +30,20 @@ def test_on_click_is_bomb(root):
 
     # Verifique se o jogo termina (derrota) após o clique na zona
     assert game.game_over is True, "O jogo não terminou após o clique na zona com uma bomba."
+
+
+def test_on_click_has_adjacent_bombs(root):
+    # Defina um cenário onde a célula (1, 1) tem bombas adjacentes
+    zone_row = 1
+    zone_col = 1
+    game = show_game("Fácil", show_difficulty_menu_function, root)
+
+    game.field[zone_row - 1][zone_col - 1] = -1
+    game.field[zone_row - 1][zone_col] = -1
+
+    game.on_button_click(zone_row, zone_col)
+
+    button_text = game.buttons[zone_row][zone_col]["text"]
+    expected_text = str(8)  # Número de bombas adjacentes
+    assert button_text == "💣", f"A célula clicada não exibe o texto correto. Esperado: {expected_text}, Obtido: {button_text}"
+
