7e26342484b41d68bd2d8e36c6dfdcf9e2607672
crishard <crislantorres9@gmail.com>
2023-10-06 10:23:45 -0300

    Testes de abandono de partidas

---
+++ tests/abandon_game_test.py
@@ -0,0 +1,49 @@
+import datetime
+import pytest
+from tkinter import Tk
+from campo_minado import CampoMinado
+
+@pytest.fixture
+def campo_minado():
+    root = Tk()
+    root.withdraw()
+    game = CampoMinado(root, 8, 8, 1, None)
+    yield game
+    root.quit()
+
+def test_abandon_game_game_over(campo_minado, mocker):
+    mocker.patch.object(campo_minado, 'show_difficulty_menu', return_value=None)
+    campo_minado.abandon_game()
+
+    # game_over inicia como false e altera para true no inicio da partida, e vira false apos derrota ou abandono
+    assert campo_minado.game_over is not True
+
+def test_abandon_game_paused(campo_minado, mocker):
+    mocker.patch.object(campo_minado, 'show_difficulty_menu', return_value=None)
+    campo_minado.paused = True
+    campo_minado.abandon_game()
+
+    # game_over inicia como false e altera para true no inicio da partida, e vira false apos derrota ou abandono
+    assert campo_minado.game_over is not  True
+
+def test_abandon_game_after_long_time(campo_minado, mocker):
+    campo_minado.start_time = campo_minado.pause_start_time = mocker.Mock()
+    campo_minado.start_time = datetime.datetime.now() - datetime.timedelta(hours=10)
+    campo_minado.abandon_game()
+
+    assert campo_minado.game_over is not True
+
+
+def test_abandon_game_game_not_started(campo_minado, mocker):
+    campo_minado.start_time = campo_minado.pause_start_time = mocker.Mock()
+    campo_minado.started = False
+    campo_minado.abandon_game()
+
+    assert campo_minado.game_over is not True
+
+
+def test_abandon_game_create(campo_minado, mocker):
+    campo_minado.start_time = campo_minado.pause_start_time = mocker.Mock()
+    campo_minado.abandon_game()
+
+    assert campo_minado.game_over is not True
