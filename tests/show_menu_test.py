1442476b6201df05bd44e223ffa74789e5e0df0c
crishard <crislantorres9@gmail.com>
2023-11-06 09:20:38 -0300

    Testando se o meu de escolha de nivel esta sendo criado corretametne

---
+++ tests/show_menu_test.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from functions.show_difficulty_menu import show_difficulty_menu_function
+
+def test_show_difficulty_menu_creation():
+    root = tk.Tk()
+    show_difficulty_menu_function(root, lambda x, y: None) 
+    children = root.winfo_children()
+    frame = children[0]
+    labels = frame.winfo_children()[0:2] 
+    buttons = frame.winfo_children()[2:5]  
+    assert len(buttons) == 3
+
