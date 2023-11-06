d642e76c1fa69047494719e58c60c1abfbe1379c
crishard <crislantorres9@gmail.com>
2023-11-06 09:26:51 -0300

    Testando se o botao de nivel itermediario e dificil estao sendo criados

--- tests/show_menu_test.py
+++ tests/show_menu_test.py
@@ -17,3 +17,18 @@ def test_easy_button_creation():
     children = root.winfo_children()[0].winfo_children()[2] 
     if children.winfo_exists(): 
         assert children["text"] == "Fácil"
+
+def test_intermediate_button_creation():
+    root = tk.Tk()
+    show_difficulty_menu_function(root, lambda x, y: None)
+    children = root.winfo_children()[0].winfo_children()[3]  # O índice 3 representa o botão "Intermediário"
+    if children.winfo_exists():
+        assert children["text"] == "Intermediário"
+
+
+def test_hard_button_creation():
+    root = tk.Tk()
+    show_difficulty_menu_function(root, lambda x, y: None)
+    children = root.winfo_children()[0].winfo_children()[4]  # O índice 4 representa o botão "Difícil"
+    if children.winfo_exists():
+        assert children["text"] == "Difícil"
