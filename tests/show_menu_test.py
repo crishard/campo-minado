3086c562add36d0648f588ba7ee3b572e5110194
crishard <crislantorres9@gmail.com>
2023-11-06 09:25:57 -0300

    Testando se o botao facil e criado

--- tests/show_menu_test.py
+++ tests/show_menu_test.py
@@ -10,3 +10,10 @@ def test_show_difficulty_menu_creation():
     buttons = frame.winfo_children()[2:5]  
     assert len(buttons) == 3
 
+
+def test_easy_button_creation():
+    root = tk.Tk()
+    show_difficulty_menu_function(root, lambda x, y: None) 
+    children = root.winfo_children()[0].winfo_children()[2] 
+    if children.winfo_exists(): 
+        assert children["text"] == "Fácil"
